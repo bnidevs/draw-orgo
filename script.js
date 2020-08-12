b7ad26518ee41ceb4a0531b46ba4070266847067
Bill Ni <bill.8ni@gmail.com>
2020-08-12 00:24:53 -0400

    select and delete working, connection display broken

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var selected = null;
+
 window.onload = () => {
   elements = document.getElementsByClassName("element");
   for(e of elements){
@@ -10,7 +12,7 @@ window.onload = () => {
   grid.className = "grid";
 
   var grid_arr = [];
-  var cnxns = {};
+  var cnxns = [];
 
   var x = 2;
 
@@ -33,6 +35,7 @@ window.onload = () => {
         insertion.ondragleave = revertDrag;
         insertion.ondragover = allowDrop;
         insertion.setAttribute("ondrop", "drop(event);");
+        insertion.addEventListener("click", insClick);
       }
       row_arr.push(insertion);
       row.appendChild(insertion);
@@ -69,6 +72,20 @@ window.onload = () => {
         line.setAttribute('stroke-opacity', '0.1');
         line.setAttribute('stroke-width', '2');
         svg.appendChild(line);
+
+        cnxns.push(new MutationObserver(function(mutations){
+          const first = grid_arr[i][j];
+          const second = grid_arr[i][j+1];
+          const connecting = line;
+          if((first.innerHTML != "" && first.innerHTML != null) && (second.innerHTML != "" && second.innerHTML != null)){
+            connecting.setAttribute('stroke-opacity', '1');
+          }else{
+            connecting.setAttribute('stroke-opacity', '0.1');
+          }
+          console.log("whoa");
+        }));
+        cnxns[cnxns.length - 1].observe(grid_arr[i][j], {characterData: true});
+        cnxns[cnxns.length - 1].observe(grid_arr[i][j+1], {characterData: true});
       }
     }
   }
@@ -126,25 +143,7 @@ window.onload = () => {
 
   org_canvas.append(svg);
 
-  // var mutObs = new MutationObserver(function(mutations){
-  //   for(var i = 1; i < grid_arr.length; i++){
-  //     for(var j = 0; j < grid_arr[0].length; j++){
-  //       var need_to_check = [[i,j,i-1,j], [i,j,i-1,j+1], [i,j,i-1,j-1]];
-  //       for(var k = 0; k < 3; i++){
-  //         if(hashCoords(need_to_check[k]) in cnxns){
-  //           var first = grid_arr[need_to_check[k][0]][need_to_check[k][1]];
-  //           var second = grid_arr[need_to_check[k][2]][need_to_check[k][3]];
-  //
-  //           if(!(first.innerHTML == "" || null) && !(second.innerHTML == "" || null)){
-  //             cnxns[hashCoords(need_to_check[k])].setAttribute('stroke-opacity', '1');
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // });
-  //
-  // mutObs.observe(document.getElementsByClassName("grid")[0], {childList: true, subtree: true});
+  document.addEventListener("keyup", del);
 }
 
 // document.getElementById("org_canvas").addEventListener("")
@@ -157,6 +156,27 @@ var getCoords = (box) => {
 
 var hashCoords = (w, x, y, z) => {
   return "" + w + "," + x + "x" + y + "," + z;
+}
+
+var insClick = (ev) => {
+  if(selected == ev.target){
+    selected.classList.remove("brdr-hl");
+    selected = null;
+  }else{
+    if(selected != null){
+      selected.classList.remove("brdr-hl");
+    }
+    selected = ev.target;
+    selected.classList.add("brdr-hl");
+  }
+}
+
+var del = (ev) => {
+  if(ev.key === 'Backspace' && selected != null){
+    selected.innerText = "";
+    selected.classList.remove("brdr-hl");
+    selected = null;
+  }
 }
 
 var allowDrop = (ev) => {
